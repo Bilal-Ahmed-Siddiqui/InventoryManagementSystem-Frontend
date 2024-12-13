5b121c2f05ff3b716942de4fadcff37cc6ea5068
Bilal Ahmed <bilalkietacc@gmail.com>
2024-12-13 16:34:14 +0500

    login api and getall categories api integrated

--- src/Pages/Categories.jsx
+++ src/Pages/Categories.jsx
@@ -1,56 +1,82 @@
-import React, { useState } from 'react'
-import Navbar from "../components/Navbar"
-import Category from '../components/Category';
-import { useNavigate } from 'react-router-dom';
-import DeleteModal from '../components/DeleteModal'
-
+import React, { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
+import Category from "../components/Category";
+import { useNavigate } from "react-router-dom";
+import DeleteModal from "../components/DeleteModal";
+import axios from "axios";
 
 const Categories = () => {
-    const [deleteModal, setDeleteModal] = useState(false)
-    const deleteMethod = () => {
-        console.log("order delete method")
-    }
-    const navigator = useNavigate()
-    const categories = [
-        { _id: 'C001', name: 'Electronics' },
-        { _id: 'C002', name: 'Clothing' },
-        { _id: 'C003', name: 'Home & Kitchen' },
-      ];
+  const [deleteModal, setDeleteModal] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const deleteMethod = () => {
+    console.log("order delete method");
+  };
+  const navigator = useNavigate();
+  // const categories = [
+  //     { _id: 'C001', name: 'Electronics' },
+  //     { _id: 'C002', name: 'Clothing' },
+  //     { _id: 'C003', name: 'Home & Kitchen' },
+  //   ];
+
+  const getAllCategories = async () => {
+    axios
+      .get("http://localhost:300/category/getall", {
+        headers: {
+          token: localStorage.getItem("token"),
+        },
+      })
+      .then((response) => {
+        setCategories(response.data);
+      })
+      .catch((err) => {
+        console.log("some error occured", err);
+      });
+  };
+
+  useEffect(() => {
+    getAllCategories();
+  }, []);
+
   return (
-<>
-<div className="flex w-full">
-    <Navbar />
-    <div className="flex-1 ml-48">
-        <div className="mb-5 px-3 flex h-16 items-center justify-between sticky top-0 bg-white z-10 shadow-md">
+    <>
+      <div className="flex w-full">
+        <Navbar />
+        <div className="flex-1 ml-48">
+          <div className="mb-5 px-3 flex h-16 items-center justify-between sticky top-0 bg-white z-10 shadow-md">
             <h1 className="text-2xl font-bold">Categories</h1>
             <button
-                className="py-2 px-4 rounded-md border-2 border-[#24cdba] text-[#24cdba] hover:bg-[#24cdba] hover:text-white "
-                onClick={()=>{navigator("/categories/create")}}
+              className="py-2 px-4 rounded-md border-2 border-[#24cdba] text-[#24cdba] hover:bg-[#24cdba] hover:text-white "
+              onClick={() => {
+                navigator("/categories/create");
+              }}
             >
-                Create New category
+              Create New category
             </button>
+          </div>
+
+          <div className="mx-3">
+            {categories.map((category) => (
+              <div>
+                <Category
+                  key={category._id}
+                  category={category}
+                  setDeleteModal={setDeleteModal}
+                />
+                {deleteModal && (
+                  <DeleteModal
+                    setDeleteModal={setDeleteModal}
+                    deleteMethod={deleteMethod}
+                    entity="Category"
+                    entityName={category._id}
+                  />
+                )}
+              </div>
+            ))}
+          </div>
         </div>
+      </div>
+    </>
+  );
+};
 
-
-        <div className="mx-3">
-                        {categories.map((category) => (
-                            <div>
-                                <Category
-                                    key={category._id}
-                                    category={category}
-                                    setDeleteModal={setDeleteModal}
-                                />
-                                {deleteModal && <DeleteModal setDeleteModal={setDeleteModal} deleteMethod={deleteMethod} entity="Category" entityName={category._id} />}
-
-                            </div>
-                        ))}
-                    </div>
-    </div>
-</div>
-
-</>
-
-  )
-}
-
-export default Categories
+export default Categories;
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
-import LoginImage from "../assets/LoginImage.png"
-
+import React, { useState } from "react";
+import LoginImage from "../assets/LoginImage.png";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const navigator = useNavigate();
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+
+    console.log(formData);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:300/user/login", formData)
+      .then(response => {
+        localStorage.setItem("token", response.data.token);
+        navigator("/");
+      })
+      .catch(err => {
+        console.log("some error occured", err);
+      });
+  };
+
   return (
     <div className="flex min-h-screen">
       <div className="w-1/2 bg-gray-200 flex items-center justify-center">
-        <img src={LoginImage} alt="Placeholder Image" className="object-cover h-full w-full p-12" />
+        <img
+          src={LoginImage}
+          alt="Placeholder Image"
+          className="object-cover h-full w-full p-12"
+        />
       </div>
 
       <div className="w-1/2 flex items-center justify-center">
@@ -13,7 +46,12 @@ const Login = () => {
           <h1 className="text-3xl font-bold mb-6">Login</h1>
           <form className="space-y-4">
             <div>
-              <label for="email" className="block text-sm font-medium text-gray-700">Email</label>
+              <label
+                for="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Email
+              </label>
               <input
                 type="email"
                 id="email"
@@ -21,11 +59,17 @@ const Login = () => {
                 className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
                 placeholder="Enter your email"
                 required
+                onChange={handleChange}
               />
             </div>
 
             <div>
-              <label for="password" className="block text-sm font-medium text-gray-700">Password</label>
+              <label
+                for="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
               <input
                 type="password"
                 id="password"
@@ -33,13 +77,16 @@ const Login = () => {
                 className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
                 placeholder="Enter your password"
                 required
+                onChange={handleChange}
               />
             </div>
 
             <div>
               <button
                 type="submit"
-                className="w-full bg-[#01e1c6]  py-2 px-4 rounded-md hover:bg-[#24cdba] focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50">
+                onClick={handleLogin}
+                className="w-full bg-[#01e1c6]  py-2 px-4 rounded-md hover:bg-[#24cdba] focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-50"
+              >
                 Login
               </button>
             </div>
@@ -47,8 +94,7 @@ const Login = () => {
         </div>
       </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Login
+export default Login;
